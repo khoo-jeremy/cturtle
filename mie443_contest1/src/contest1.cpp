c7240de52c96be94bc3c5aff37cd6ae95e164421
leah-zhang <leahzhang1997@yahoo.com>
2020-01-23 13:11:12 -0500

    first commit

---
+++ mie443_contest1/src/contest1.cpp
@@ -0,0 +1,100 @@
+#include <ros/console.h>
+#include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
+#include <kobuki_msgs/BumperEvent.h>
+#include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+
+#include <stdio.h>
+#include <cmath>
+
+#include <chrono>
+
+// #define N_BUMPER(3)
+#define RAD2DEG(rad) ((rad) * 180 / M_PI)
+#define DEG2RAD(deg) ((deg) * M_PI / 180)
+
+float angular = 0.0;
+float linear = 0.0;
+
+float posX= 0.0, posY = 0.0, yaw = 0.0;
+float minLaserDist = std::numeric_limits<float>::infinity();
+int32_t nLasers= 0, desiredNLasers= 0, desiredAngle= 5;
+
+uint8_t bumper[3]= {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
+
+void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
+{
+	bumper[msg->bumper]= msg->state;
+}
+
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+	nLasers= (msg->angle_max-msg->angle_min)/msg->angle_increment;
+    desiredNLasers= DEG2RAD(desiredAngle)/msg->angle_increment;
+    // ROS_INFO("Size of laser scan array: %i and size of offset: %i", nLasers, desiredNLasers);
+
+    if(desiredAngle * M_PI/180 <msg->angle_max && -desiredAngle * M_PI/180>msg->angle_min)
+    {
+        for(uint32_t laser_idx= nLasers/2-desiredNLasers; laser_idx < nLasers/2 + desiredNLasers; ++laser_idx)
+        {
+            minLaserDist= std::min(minLaserDist, msg->ranges[laser_idx]);
+        }
+    }else
+    {
+        for(uint32_t laser_idx= 0; laser_idx < nLasers; ++laser_idx)
+        {
+            minLaserDist= std::min(minLaserDist, msg->ranges[laser_idx]);
+        }
+    }
+    ROS_INFO("Minimum distance from object: %f", minLaserDist);
+}
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+    posX = msg->pose.pose.position.x;
+    posY = msg->pose.pose.position.y;
+    yaw = tf::getYaw(msg->pose.pose.orientation);
+    tf::getYaw(msg->pose.pose.orientation);
+    // ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees", posX, posY, yaw, RAD2DEG(yaw));
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "image_listener");
+    ros::NodeHandle nh;
+
+    ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
+    ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
+    ros::Subscriber odom= nh.subscribe("odom",1,&odomCallback);
+
+    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
+
+    ros::Rate loop_rate(10);
+
+    geometry_msgs::Twist vel;
+
+    // contest count down timer
+    std::chrono::time_point<std::chrono::system_clock> start;
+    start = std::chrono::system_clock::now();
+    uint64_t secondsElapsed = 0;
+
+    float angular = 0.0;
+    float linear = 0.0;
+
+    while(ros::ok() && secondsElapsed <= 480) {
+        ros::spinOnce();
+        //fill with your code
+
+        vel.angular.z = angular;
+        vel.linear.x = linear;
+        vel_pub.publish(vel);
+
+        // The last thing to do is to update the timer.
+        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
