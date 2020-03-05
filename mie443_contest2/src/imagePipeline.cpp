6a71f80c6485d19a6171db234b586fa7fc67c58c
leah-zhang <leahzhang1997@yahoo.com>
2020-03-05 11:22:37 -0500

    working open cv

--- mie443_contest2/src/imagePipeline.cpp
+++ mie443_contest2/src/imagePipeline.cpp
@@ -48,25 +48,31 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {
         /***YOUR CODE HERE***/
         Mat img_bw;
 
+        //convert to grayscale
         cv::cvtColor(img, img_bw, cv::COLOR_BGR2GRAY);
+        //flip horizontally
+        // flip(img_bw, img_bw, +1);
+
+        Mat img_scene= img_bw;
+        Mat img_object= boxes.templates[0];
 
         int minHessian = 400;
 
         Ptr<SURF> detector = SURF::create(minHessian);
 
-        std::vector<KeyPoint> keypoints_img, keypoints_box;
-        Mat descriptors_img, descriptors_box;
+        std::vector<KeyPoint> keypoints_object, keypoints_scene;
+        Mat descriptors_object, descriptors_scene;
 
-        detector->detectAndCompute(boxes.templates[0], Mat(), keypoints_img, descriptors_img);
-        detector->detectAndCompute(img_bw, Mat(), keypoints_box, descriptors_box);
+        detector->detectAndCompute(img_object, Mat(), keypoints_object, descriptors_object);
+        detector->detectAndCompute(img_scene, Mat(), keypoints_scene, descriptors_scene);
 
         FlannBasedMatcher matcher;
         std::vector< DMatch > matches;
-        matcher.match(descriptors_img, descriptors_box, matches);
+        matcher.match(descriptors_object, descriptors_scene, matches);
         double max_dist = 0;
         double min_dist = 100;
 
-        for(int i = 0;i < descriptors_img.rows;i++){
+        for(int i = 0;i < descriptors_object.rows;i++){
             double dist = matches[i].distance;
             if(dist < min_dist){
                 min_dist = dist;
@@ -74,28 +80,26 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {
             if(dist > max_dist){
                 max_dist = dist;
             }
-        }
+            }
 
         std::vector<DMatch> good_matches;
 
-        for(int i= 0; i < descriptors_img.rows;i++){
+        for(int i= 0; i < descriptors_object.rows;i++){
             if(matches[i].distance < 3 * min_dist){
                 good_matches.push_back(matches[i]);
             }
         }
 
-        //scene= boxes.templates[0], object=img
-
         Mat img_matches;
-        drawMatches(img_bw, keypoints_img, boxes.templates[0], keypoints_box, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+        drawMatches(img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
         std::vector<Point2f> obj;
         std::vector<Point2f> scene;
 
         for(int i= 0;i < good_matches.size();i++){
             //Get the keypoints from the good matches
-            obj.push_back(keypoints_img[good_matches[i].queryIdx].pt);
-            scene.push_back(keypoints_box[good_matches[i].trainIdx].pt);
+            obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
+            scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
         }
 
         Mat H= findHomography(obj, scene, RANSAC);
@@ -103,22 +107,22 @@ int ImagePipeline::getTemplateID(Boxes& boxes) {
         //Get the corners from the image_1 ( the object to be "detected" )
         std::vector<Point2f> obj_corners(4);
         obj_corners[0]= cvPoint(0,0);
-        obj_corners[1]= cvPoint(img_bw.cols, 0);
-        obj_corners[2]= cvPoint(img_bw.cols, img_bw.rows);
-        obj_corners[3]= cvPoint(0, img_bw.rows);
+        obj_corners[1]= cvPoint(img_object.cols, 0);
+        obj_corners[2]= cvPoint(img_object.cols, img_object.rows);
+        obj_corners[3]= cvPoint(0, img_object.rows);
         std::vector<Point2f> scene_corners(4);
 
         perspectiveTransform(obj_corners, scene_corners, H);
 
         // Draw lines between the corners (the mapped object in the scene- image_2)
-        line(img_matches, scene_corners[0] + Point2f(img_bw.cols, 0), scene_corners[1] + Point2f(img_bw.cols, 0), Scalar(0, 255, 0), 4);
-        line(img_matches, scene_corners[1] + Point2f(img_bw.cols, 0), scene_corners[2] + Point2f(img_bw.cols, 0), Scalar(0, 255, 0), 4);
-        line(img_matches, scene_corners[2] + Point2f(img_bw.cols, 0), scene_corners[3] + Point2f(img_bw.cols, 0), Scalar(0, 255, 0), 4);
-        line(img_matches, scene_corners[3] + Point2f(img_bw.cols, 0), scene_corners[0] + Point2f(img_bw.cols, 0), Scalar(0, 255, 0), 4);
+        line(img_matches, scene_corners[0] + Point2f(img_object.cols, 0), scene_corners[1] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+        line(img_matches, scene_corners[1] + Point2f(img_object.cols, 0), scene_corners[2] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+        line(img_matches, scene_corners[2] + Point2f(img_object.cols, 0), scene_corners[3] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+        line(img_matches, scene_corners[3] + Point2f(img_object.cols, 0), scene_corners[0] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
         
         imshow("Good Matches & Object detection", img_matches);
 
-        cv::imshow("view", img_bw);
+        // cv::imshow("view", img_bw);
         // cv::imshow("view", boxes.templates[0]);
         cv::waitKey(10);
     }  
