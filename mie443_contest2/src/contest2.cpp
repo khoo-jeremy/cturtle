fc4bfc4f7400cead80a39489a5b3ae05a4644169
khoo-jeremy <jperson39@gmail.com>
2020-03-25 23:14:12 -0400

    reduce approach range to 0.6m

--- mie443_contest2/src/contest2.cpp
+++ mie443_contest2/src/contest2.cpp
@@ -43,8 +43,8 @@ int main(int argc, char** argv) {
     std::vector<std::vector<float>> coordinates;
     for(int i=0;i<5;i++)
     {
-        float x1 = boxes.coords[i][0]+ 0.8 * cos(boxes.coords[i][2]);
-        float y1 = boxes.coords[i][1]+ 0.8 * sin(boxes.coords[i][2]);
+        float x1 = boxes.coords[i][0]+ 0.6 * cos(boxes.coords[i][2]);
+        float y1 = boxes.coords[i][1]+ 0.6 * sin(boxes.coords[i][2]);
         float z1;
         if(boxes.coords[i][2] > 0)
             z1 = boxes.coords[i][2] - M_PI;
